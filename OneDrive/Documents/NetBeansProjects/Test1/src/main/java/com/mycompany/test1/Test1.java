6bc220f5be3bda000a83c469d32379a9811573a3
nguyentienduan22012003@gmail.com <nguyentienduan22012003@gmail.com>
2024-03-01 20:38:18 +0700

    first Commit

---
+++ OneDrive/Documents/NetBeansProjects/Test1/src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test1;
+
+/**
+ *
+ * @author nguye
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
