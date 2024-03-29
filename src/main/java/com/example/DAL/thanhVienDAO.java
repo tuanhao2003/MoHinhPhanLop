7faa451376d5dbc2523abbb2dd521857929df2e1
unknown <nhtuanhao@gmail.com>
2024-03-29 11:23:56 +0700

    update thanhvienDAO

--- src/main/java/com/example/DAL/thanhVienDAO.java
+++ src/main/java/com/example/DAL/thanhVienDAO.java
@@ -5,9 +5,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.*;
 
 import java.util.*;
 
+@SuppressWarnings("rawtypes")
 public class thanhVienDAO {
     private SessionFactory sf;
     private Session sess;
@@ -19,13 +21,13 @@ public class thanhVienDAO {
         this.trans = null;
     }
 
-    @SuppressWarnings("rawtypes")
     public List<thanhVien> listMember(){
         try {
             this.sess = this.sf.openSession();
             this.trans = this.sess.beginTransaction();
             List<thanhVien> members = new ArrayList<thanhVien>();
-            List datas = this.sess.createQuery("from thanhVien").list();
+            Query query = this.sess.createQuery("from thanhVien");
+            List datas = query.list();
             
             for(Iterator iterator = datas.iterator();iterator.hasNext();){
                 thanhVien mem = (thanhVien) iterator.next();
@@ -33,6 +35,31 @@ public class thanhVienDAO {
             }
             this.trans.commit();
             return members;
+        } catch (HibernateException he) {
+            if(trans != null){
+                trans.rollback();
+            }
+            he.printStackTrace();
+        } finally{
+            this.sess.close();
+        }
+        return null;
+    }
+
+    public thanhVien member(int matv){
+        try {
+            this.sess = this.sf.openSession();
+            this.trans = this.sess.beginTransaction();
+            thanhVien mem = new thanhVien();
+            Query query = this.sess.createQuery("from thanhVien mem where mem.matv= :matv");
+            query.setParameter("matv", matv);
+            List datas = query.list();
+            
+            for(Iterator iterator = datas.iterator();iterator.hasNext();){
+                mem = (thanhVien) iterator.next();
+            }
+            this.trans.commit();
+            return mem;
         } catch (HibernateException he) {
             if(trans != null){
                 trans.rollback();
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,5 +9,9 @@ public class Main {
         for(thanhVien i : memdao.listMember()){
             System.out.println("\nMSSV: "+i.getMatv()+ "\nTen: " +i.getHoten()+"\nKhoa: "+i.getkhoa()+"\nNganh: "+i.getNganh()+"\nSDT: "+i.getSdt());
         }
+        
+        thanhVien a = memdao.member(2147483647);
+        System.out.println("\nMSSV: "+a.getMatv()+ "\nTen: " +a.getHoten()+"\nKhoa: "+a.getkhoa()+"\nNganh: "+a.getNganh()+"\nSDT: "+a.getSdt());
+        
     }
 }
